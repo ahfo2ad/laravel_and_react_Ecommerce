81f27a3cb8442530e41688813926236626e03333
Ahmed fouad <ahfouad12@gmail.com>
2022-08-08 20:42:22 +0200

    making slider

---
+++ user/ecom/src/components/home/NewArrival.jsx
@@ -0,0 +1,113 @@
+import React, { Component, Fragment } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+class NewArrival extends Component {
+    render() {
+
+        var settings = {
+            dots: true,
+            infinite: false,
+            speed: 500,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            initialSlide: 0,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+
+        return (
+            <Fragment>
+                <Container fluid className='text-center'>
+                    <div className='section-title text-center mb-55'>
+                        <h2>new arrival</h2>
+                        <p>some of our exclusive collection, you may like</p>
+                    </div>
+                    <Row>
+                        <Slider {...settings}>
+                            <div>
+                                <h3>1</h3>
+                            </div>
+                            <div>
+                                <Card className='card image-box w-100'>
+                                    <img className='center w-75' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                    <Card.Body>
+                                        <p className='product-name-on-card'>realme phone</p>
+                                        <p className='product-price-on-card'>price: $150</p>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div>
+                                <h3>3</h3>
+                            </div>
+                            <div>
+                                <Card className='card image-box w-100'>
+                                    <img className='center w-75' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                    <Card.Body>
+                                        <p className='product-name-on-card'>realme phone</p>
+                                        <p className='product-price-on-card'>price: $150</p>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div>
+                                <h3>5</h3>
+                            </div>
+                            <div>
+                                <Card className='card image-box w-100'>
+                                    <img className='center w-75' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                    <Card.Body>
+                                        <p className='product-name-on-card'>realme phone</p>
+                                        <p className='product-price-on-card'>price: $150</p>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                            <div>
+                                <h3>7</h3>
+                            </div>
+                            <div>
+                                <Card className='card image-box w-100'>
+                                    <img className='center w-75' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                    <Card.Body>
+                                        <p className='product-name-on-card'>realme phone</p>
+                                        <p className='product-price-on-card'>price: $150</p>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </Slider>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default NewArrival;
