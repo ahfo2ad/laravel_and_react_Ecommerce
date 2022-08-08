affbda7487713436bcc817899ba4f1edef51bfce
Ahmed fouad <ahfouad12@gmail.com>
2022-08-08 17:50:20 +0200

    adding categories

---
+++ user/ecom/src/components/home/Categories.jsx
@@ -0,0 +1,98 @@
+import React, { Component, Fragment } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+export class Categories extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container fluid className='text-center'>
+                    <div className='section-title text-center mb-55'>
+                        <h2>categories</h2>
+                        <p>some of our exclusive collection, you may like</p>
+                    </div>
+                    <Row>
+                        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                            <Row>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                
+                            </Row>
+                            
+                        </Col>
+
+                        <Col key={1} xl={6} lg={6} md={2} sm={12} xs={12}>
+                            <Row>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col className='p-0' key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
+                                    <Card className='h-100 w-100 text-center'>
+                                        <Card.Body>
+                                            <img className='center' src="https://images-na.ssl-images-amazon.com/images/I/61K8lMf1QnL.__AC_SX300_SY300_QL70_FMwebp_.jpg" />
+                                            <h5 className='category-name'>Mobile</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            </Row>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default Categories;
