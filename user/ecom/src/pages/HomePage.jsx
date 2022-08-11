594e03fa4d2105dfd02c06ab1cb5042dba7b95a4
Ahmed fouad <ahfouad12@gmail.com>
2022-08-11 17:43:24 +0200

    nav menu mobile p1

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import NavMenuDesktop from '../components/common/NavMenuDesktop';
+import NavMenuMobile from '../components/common/NavMenuMobile';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
 import FeaturedProducts from '../components/home/FeaturedProducts';
@@ -10,13 +11,14 @@ export class HomePage extends Component {
     render() {
         return (
             <Fragment>
-                <NavMenuDesktop />
+                <NavMenuMobile />
+                {/* <NavMenuDesktop /> */}
                 <HomeTop />
                 <FeaturedProducts />
                 <NewArrival />
                 <Categories />
                 <Collection />
-                
+
             </Fragment>
         );
     }
