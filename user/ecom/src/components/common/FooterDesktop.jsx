4649d3de985142db1920b13a91fa6a2a89ea29f9
Ahmed fouad <ahfouad12@gmail.com>
2022-08-16 22:44:37 +0200

    finish footer desktop

--- user/ecom/src/components/common/FooterDesktop.jsx
+++ user/ecom/src/components/common/FooterDesktop.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Apple from '../../assets/images/apple.png'
+import Google from '../../assets/images/google.png'
 export class FooterDesktop extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Fragment>
+                <div className='footerback m-0 mt-5 pt-4 py-4 shadow-sm'>
+                    <Container>
+                        <Row className='px-0'>
+                            <Col lg={3} md={3} sm={6} xs={12}>
+                                <h5 className='footer-menu-title text-uppercase'>office address</h5>
+                                <p>Location Address</p>
+                                <p>Eamil: ahfouad12@gmail.com</p>
+                                <h5 className='footer-menu-title'>social links</h5>
+                                <a href="#"><i className='fab fa-facebook m-1 h4'></i></a>
+                                <a href="#"><i className='fab fa-instagram m-1 h4'></i></a>
+                                <a href="#"><i className='fab fa-twitter m-1 h4'></i></a>
+                            </Col>
+                            <Col lg={3} md={3} sm={6} xs={12}>
+                                <h5 className='footer-menu-title text-uppercase'>the company</h5>
+                                <Link to={"/"} className="footer-link">about us</Link> <br />
+                                <Link to={"/"} className="footer-link">company profile</Link> <br />
+                                <Link to={"/"} className="footer-link">contact us</Link> <br />
+                                
+                            </Col>
+                            <Col lg={3} md={3} sm={6} xs={12}>
+                                <h5 className='footer-menu-title text-uppercase'>more info</h5>
+                                <Link to={"/"} className="footer-link">how to purchase</Link> <br />
+                                <Link to={"/"} className="footer-link">privacy policy</Link> <br />
+                                <Link to={"/"} className="footer-link">refund policy</Link> <br />
+                            </Col>
+                            <Col lg={3} md={3} sm={6} xs={12}>
+                                <h5 className='footer-menu-title text-uppercase'>download apps</h5>
+                                <a href='#'><img src={Apple} /></a>
+                                <a href='#'><img className='mt-2' src={Google} /></a>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </Fragment>
         );
     }
 }
